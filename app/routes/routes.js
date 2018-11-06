1bf66425e347efe0630f60e0ce8a50052adc7f44
Christian Bush <cbb330@msstate.edu>
2018-11-06 12:15:12 -0600

    last commit didn't include frontend

--- app/routes/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function(app) {
-  var coachApp = require('./coachAppController');
-
-  // coachApp Routes
-  app.route('/scan')
-    .get(coachApp.getDeviceInfo);
-};
