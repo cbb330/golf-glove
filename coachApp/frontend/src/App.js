2b08cabfd8688991769cf33355659e3f17ab5ffd
Jack Fletcher <jdfletch97@gmail.com>
2018-11-06 17:30:02 -0600

    Add chart rendering via victory with test data

--- coachApp/frontend/src/App.js
+++ coachApp/frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import TestChart from './TestChart.js'
 
 class App extends Component {
   render() {
-      return <div>Hello World!</div>
+    return (
+      <div>
+        <h1>Coach App</h1>
+        <TestChart />
+      </div>
+    )
   }
 }
 
