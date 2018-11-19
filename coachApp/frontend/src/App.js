6aeb392c1e665801cd24d2f91b0775076da4c300
Jack Fletcher <jdfletch97@gmail.com>
2018-11-19 12:59:13 -0600

    Comment out unused http request package and route

--- coachApp/frontend/src/App.js
+++ coachApp/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios'; // http request package
 
 import TestChart from './TestChart.js';
 import testData from './data/test-data.json';
@@ -9,40 +9,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      response: undefined,
-      socket: undefined
+      response: undefined
     };
   }
   
   componentDidMount() {
-    this.getHello();
-    // this.getSocket();
     this.sendScan();
   }
-  
-  getHello() {
-    axios.get('http://170.253.147.206:4000/hello') //to access Christian Bush's rsbpi
-      .then((response) => {
-        this.setHello(response.data.time);
-        // console.log(response.data)
-      });
-  }
-  
-  // TODO: fix this
-  setHello(e) {
-    this.setState({
-      response: e
-    });
-  }
-  
-  getSocket() {
-    const ws = new WebSocket("ws://170.253.147.206:8080"); //to access Christian Bush's rsbpi
-    ws.onmessage = event => {
-      this.setState({ socket: JSON.parse(event.data) });
-      console.log(this.state.socket);
-    };
-  };
 
+  // http request for future needs
+  // getHello() {
+  //   axios.get('http://170.253.147.206:4000/hello') //to access Christian Bush's rsbpi
+  //     .then((response) => {
+  //       this.setHello(response.data.time);
+  //       // console.log(response.data)
+  //     });
+  // }
+
+  // setHello(e) {
+  //   this.setState({
+  //     response: e
+  //   });
+  // }
+  
   sendScan() {
     const ws = new WebSocket("ws://170.253.147.206:8080"); //to access Christian Bush's rsbpi
     
@@ -63,19 +52,9 @@ class App extends Component {
 
 
   render() {
-
-    // if ((this.state.response === undefined) && (this.state.socket === undefined)) {
-    //   return (
-    //       <div style={{height: 600, width: 1000}}>
-    //         <h1>Still fetching data</h1>
-    //         <TestChart data={testData} />
-    //       </div>
-    //   )
-    // }
-    // else {
     return (
         <div style={{height: 600, width: 1000}}>
-          {this.state.response === undefined && this.state.socket === undefined ?
+          {this.state.response === undefined ?
             <h1>Still fetching data</h1> :
             <div>
               <h1>{this.state.response}</h1>
