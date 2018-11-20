1b304524bf3d686861239b5b7608d87ecc6a4481
Jack Fletcher <jdfletch97@gmail.com>
2018-11-20 14:40:38 -0600

    Add frontend routes for connect/disconnect device and start/stop data

--- coachApp/frontend/src/App.js
+++ coachApp/frontend/src/App.js
@@ -9,12 +9,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isAcceptingData: false,
+      isConnectedToDevice: false,
       response: undefined
     };
+
+    this.socket = new WebSocket("ws://170.253.147.206:8081");
   }
   
   componentDidMount() {
-    this.sendScan();
+    this.socket.onmessage = (event) => {
+      // TODO: handle responses from server
+      // TODO: responses include data, errors with connection, and general updates
+      const response = JSON.parse(event.data);
+      console.log(response);
+      this.setState({response});
+    };
   }
 
   // http request for future needs
@@ -32,34 +42,93 @@ class App extends Component {
   //   });
   // }
   
-  sendScan() {
-    const ws = new WebSocket("ws://170.253.147.206:8080"); //to access Christian Bush's rsbpi
-    
-    // Send the msg object as a JSON-formatted string.
-    ws.onopen = (event) => {
-      console.log('we open');
-      // Construct a msg object containing the data the server needs to process the message from the chat client.
-      var msg = {
-        type: "scanOn"
-      };
-      console.log(JSON.stringify(msg));
-      ws.send(JSON.stringify(msg));
-    };
-    ws.onmessage = (event) => {
-      console.log(event.data);
-    };
-  };
+  handleConnect() {
+    this.connectDevice();
+    this.setState({
+      isConnectedToDevice: true
+    });
+  }
 
+  handleDisconnect() {
+    this.disconnectDevice();
+    this.setState({
+      isConnectedToDevice: false
+    });
+  }
+
+  handleStartAcceptingData() {
+    this.startDataReception();
+    this.setState({
+      isAcceptingData: true
+    });
+  }
+
+  handleStopAcceptingData() {
+    this.stopDataReception();
+    this.setState({
+      isAcceptingData: false
+    });
+  }
+
+  connectDevice() {
+    // construct connect message object
+    const message = {type: "connect"};
+    console.log('sending following connect message to backend:');
+    console.log(message);
+    // send message as a JSON-formatted string
+    this.socket.send(JSON.stringify(message));
+  }
+
+  disconnectDevice() {
+    // construct disconnect message object
+    const message = {type: "disconnect"};
+    console.log('sending following disconnect message to backend:');
+    console.log(message);
+    // send message as a JSON-formatted string
+    this.socket.send(JSON.stringify(message));
+  }
+
+  startDataReception() {
+    // construct start message object
+    const message = {type: "start"};
+    console.log('sending following start message to backend:');
+    console.log(message);
+    // send message as a JSON-formatted string
+    this.socket.send(JSON.stringify(message));
+  }
+
+  stopDataReception() {
+    // construct stop message object
+    const message = {type: "stop"};
+    console.log('sending following start message to backend:');
+    console.log(message);
+    // send message as a JSON-formatted string
+    this.socket.send(JSON.stringify(message));
+  }
 
   render() {
     return (
         <div style={{height: 600, width: 1000}}>
+          <button onClick={(e) => this.handleConnect(e)} disabled={this.state.isConnectedToDevice}>
+            Connect
+          </button>
+          <button onClick={(e) => this.handleDisconnect(e)} disabled={!this.state.isConnectedToDevice}>
+            Disconnect
+          </button>
+          <button onClick={(e) => this.handleStartAcceptingData(e)} disabled={this.state.isAcceptingData}>
+            Start
+          </button>
+          <button onClick={(e) => this.handleStopAcceptingData(e)} disabled={!this.state.isAcceptingData}>
+            Stop
+          </button>
+          {/* <p>{`Socket state: ${this.socket.readyState ? "connected" : "disconnected"}`}</p> */}
+          <p>{`Connection state: ${this.state.isConnectedToDevice ? "connected" : "disconnected"}`}.</p>
+          <p>{`Data state: ${this.state.isAcceptingData ? "accepting" : "not accepting"}`}.</p>
           {this.state.response === undefined ?
             <h1>Still fetching data</h1> :
             <div>
-              <h1>{this.state.response}</h1>
-              <h1>{this.state.socket["name"]}</h1> {/* name is conditionally set in backend */}
-              <h2>{this.state.socket.uuid}</h2>
+              <h1>{`Name: ${this.state.response.name}`}</h1> {/* name is conditionally set in backend */}
+              <h2>{`Uuid: ${this.state.response.uuid}`}</h2>
             </div>
           }
           <TestChart data={testData} />
