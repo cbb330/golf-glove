855c2f6fc44930f6894350afef0e9294bee8b685
Jack Fletcher <jdfletch97@gmail.com>
2019-05-11 01:07:25 -0500

    Change database to store frame data fields as raw values instead of as json text

--- coachApp/backend/db/db.js
+++ coachApp/backend/db/db.js
@@ -24,7 +24,32 @@ class GolfGloveDb {
            This is so front end can accurately display real time vs full swing data, without the database knowing state
       * */
       this.db.run("CREATE TABLE IF NOT EXISTS swings (timestamp INTEGER, swingID INTEGER PRIMARY KEY)");
-      this.db.run("CREATE TABLE IF NOT EXISTS frames (frame TEXT, timestamp INTEGER , swingID INTEGER, PRIMARY KEY (timestamp, swingID), FOREIGN KEY (swingID) REFERENCES swings(swingID))");
+      this.db.run("CREATE TABLE IF NOT EXISTS frames (\
+        timestamp INTEGER,\
+        pressure1 INTEGER,\
+        pressure2 INTEGER,\
+        deflection INTEGER,\
+        extension INTEGER,\
+        radialDeviation INTEGER,\
+        ulnarDeviation INTEGER,\
+        accel1X REAL,\
+        accel1Y REAL,\
+        accel1Z REAL,\
+        gyro1X REAL,\
+        gyro1Y REAL,\
+        gyro1Z REAL,\
+        accel2X REAL,\
+        accel2Y REAL,\
+        accel2Z REAL,\
+        gyro2X REAL,\
+        gyro2Y REAL,\
+        gyro2Z REAL,\
+        swingSync INTEGER,\
+        dataAvailable INTEGER,\
+        swingID INTEGER,\
+        PRIMARY KEY (timestamp, swingID),\
+        FOREIGN KEY (swingID) REFERENCES swings(swingID)\
+      )");
       this.db.get('SELECT swingID FROM swings ORDER BY swingID DESC LIMIT 1', (err, row) => {
         if (err) console.error(err);
         else if (row) this.currSwingID = row;
@@ -38,12 +63,58 @@ class GolfGloveDb {
       this.storeSwing(frame);
     }
     else {
-	    // console.log(frame.timestamp, this.currSwingID);
-      this.db.run('INSERT INTO frames (frame, timestamp, swingID) VALUES (?, ?, ?)',
-          [JSON.stringify(frame), frame.timestamp, this.currSwingID], (err) => {
-            if (err) console.error("Frame Table ", err);
-            // else console.log("Frame stored");
-          });
+      const data = [
+        frame.timestamp,
+        frame.pressure1,
+        frame.pressure2,
+        frame.deflection,
+        frame.extension,
+        frame.radialDeviation,
+        frame.ulnarDeviation,
+        frame.imu1.accelX,
+        frame.imu1.accelY,
+        frame.imu1.accelZ,
+        frame.imu1.gyroX,
+        frame.imu1.gyroY,
+        frame.imu1.gyroZ,
+        frame.imu2.accelX,
+        frame.imu2.accelY,
+        frame.imu2.accelZ,
+        frame.imu2.gyroX,
+        frame.imu2.gyroY,
+        frame.imu2.gyroZ,
+        frame.swingSync,
+        frame.dataAvailable,
+        this.currSwingID,
+      ];
+      this.db.run('INSERT INTO frames (\
+          timestamp,\
+          pressure1,\
+          pressure2,\
+          deflection,\
+          extension,\
+          radialDeviation,\
+          ulnarDeviation,\
+          accel1X,\
+          accel1Y,\
+          accel1Z,\
+          gyro1X,\
+          gyro1Y,\
+          gyro1Z,\
+          accel2X,\
+          accel2Y,\
+          accel2Z,\
+          gyro2X,\
+          gyro2Y,\
+          gyro2Z,\
+          swingSync,\
+          dataAvailable,\
+          swingID\
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        data, (err) => {
+          if (err) console.error("Error inserting into 'Frame' Table: ", err);
+          // else console.log("Frame stored");
+        });
     }
   }
 
